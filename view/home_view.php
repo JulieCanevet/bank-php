2a9ea2c651b0cb00e1f399308c92813428e5dd24
Julie.Canevet <julie.canevet@hotmail.fr>
2017-11-14 10:41:44 +0100

    add plans

--- view/home_view.php
+++ view/home_view.php
@@ -10,31 +10,31 @@
     <p>compte n°<?php echo $value['id'] ?></p>
 
     <h4 class="color">Montant : <span class="sum"><?php echo $value['amount'] ?></span>€</h4>
-<article class="actions">
-          <form method="POST" action="index.php">
-            <input type="hidden" name="id" value="<?php echo $value['id']?>">
-            <button type="submit" name="removeForm">
-              <i class="fa fa-minus-circle" aria-hidden="true"></i>
-            </button>
-          </form>
+    <article class="actions">
+      <form method="POST" action="index.php">
+        <input type="hidden" name="id" value="<?php echo $value['id']?>">
+        <button type="submit" name="removeForm">
+          <i class="fa fa-minus-circle" aria-hidden="true"></i>
+        </button>
+      </form>
 
-          <form method="POST" action="index.php">
-            <input type="hidden" name="id" value="<?php echo $value['id']?>">
-            <button type="submit" name="transferForm"><i class="fa fa-exchange" aria-hidden="true"></i></button>
-          </form>
-          
-          <form method="POST" action="index.php">
-            <input type="hidden" name="id" value="<?php echo $value['id']?>">
-            <button type="submit" name="addMoneyForm">
-              <i class="fa fa-plus-circle" aria-hidden="true"></i>
-            </button>
-          </form>
+      <form method="POST" action="index.php">
+        <input type="hidden" name="id" value="<?php echo $value['id']?>">
+        <button type="submit" name="transferForm"><i class="fa fa-exchange" aria-hidden="true"></i></button>
+      </form>
+      
+      <form method="POST" action="index.php">
+        <input type="hidden" name="id" value="<?php echo $value['id']?>">
+        <button type="submit" name="addMoneyForm">
+          <i class="fa fa-plus-circle" aria-hidden="true"></i>
+        </button>
+      </form>
 
-          <form class="delete" method="POST" action="index.php">
-            <input type="hidden" name="id" value="<?php echo $value['id']?>"/>
-            <button type="submit" name="delete" onclick="if(!confirm('Etes-vous sur de vouloir supprimer ce compte ?')) return false;"><i class="fa fa-times" aria-hidden="true"></i></button>
-          </form>
-        </article>
+      <form class="delete" method="POST" action="index.php">
+        <input type="hidden" name="id" value="<?php echo $value['id']?>"/>
+        <button type="submit" name="delete" onclick="if(!confirm('Etes-vous sur de vouloir supprimer ce compte ?')) return false;"><i class="fa fa-times" aria-hidden="true"></i></button>
+      </form>
+    </article>
 </article>
 
   <?php }} ?>
--- view/transfer_view.php
+++ view/transfer_view.php
@@ -2,7 +2,7 @@
 <section class="transmitterCount">
     <div class="container col-12 col-sm-6 col-md-6">
         <div class="form-wrap">
-        	<h1>Numéro du compte emmeteur</h1>
+        	<h1>Numéro du compte emetteur</h1>
             <form method="post" action="index.php">
                 <div class="form-group">
                     <input type="text" name="transmitterCount" class="form-control" placeholder="numéro du compte emmeteur" value="<?php echo $_POST['id']?>">
